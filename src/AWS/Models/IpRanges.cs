22fa71ed35212c0002fb64526d7933fbee9d93c1
Guido van Rossum <guido@python.org>
2026-03-04 10:41:15 +0400

    init

---
+++ src/AWS/Models/IpRanges.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AWS.Models;
+
+public class IpRanges
+{
+    [JsonPropertyName("syncToken")]
+    public string SyncToken { get; set; }
+
+    [JsonPropertyName("createDate")]
+    public string CreateDate { get; set; }
+
+    [JsonPropertyName("prefixes")]
+    public List<PrefixDesc> Prefixes { get; set; }
+}
+
+public class PrefixDesc
+{
+    [JsonPropertyName("ip_prefix")]
+    public string IpPrefix { get; set; }
+
+    [JsonPropertyName("region")]
+    public string Region { get; set; }
+
+    [JsonPropertyName("service")]
+    public string Service { get; set; }
+
+    [JsonPropertyName("network_border_group")]
+    public string NetworkBorderGroup { get; set; }
+}
---
+++ src/AWS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using AWS.Models;
+
+const string url = "https://ip-ranges.amazonaws.com/ip-ranges.json";
+const string downloadedFileName = "downloaded_raw.json";
+const string ipsFileName = "aws_ip_ranges.json";
+
+using (var client = new HttpClient())
+{
+    await using var s = await client.GetStreamAsync(url);
+    await using var fs = new FileStream(downloadedFileName, FileMode.Create);
+    await s.CopyToAsync(fs);
+}
+
+var jsonContent = File.ReadAllText(downloadedFileName);
+var res = JsonSerializer.Deserialize<IpRanges>(jsonContent);
+
+var it = 0;
+var antiDuplicateList = new List<string>();
+if (res?.Prefixes != null)
+{
+    await using var writer = new StreamWriter(ipsFileName);
+    foreach (var prefix in res.Prefixes)
+    {
+        if (!antiDuplicateList.Contains(prefix.IpPrefix))
+        {
+            writer.WriteLine(prefix.IpPrefix);
+            antiDuplicateList.Add(prefix.IpPrefix);
+            it++;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Failed to deserialize prefixes");
+}
+
+Console.WriteLine($"Work done, parsed and converted {it} ip ranges to {ipsFileName} file");
